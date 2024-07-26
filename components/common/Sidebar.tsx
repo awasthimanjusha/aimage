9d1a422ae802ec3121dfd432b620cafc2b07bbf7
Manjusha Awasthi <manjusha.awasthi@itpeoplecorp.com>
2024-07-26 16:02:19 -0400

    Style sidebar component

--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -28,13 +28,22 @@ const Sidebar = () => {
                 return (
                   <li
                     key={link.route}
-                    className={`sidebar-nav_element group p-2 ${
+                    className={`sidebar-nav_element group ${
                       isActive
                         ? "bg-purple-gradient text-white"
                         : "text-gray-700"
                     }`}
                   >
-                    {link.label}
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
                   </li>
                 );
               })}
