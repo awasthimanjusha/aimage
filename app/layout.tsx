59ee5ee7d48faa7535754b177737adbd6d24181c
Manjusha Awasthi <manjusha.awasthi@itpeoplecorp.com>
2024-07-26 08:13:44 -0400

    Add feature to sign in and sign up and protect routes when user is not logged in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -24,6 +25,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
           {children}
         </body>
       </html>
